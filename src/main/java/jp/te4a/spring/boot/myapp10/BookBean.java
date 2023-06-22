a6300b6ad425cb3e3e328a84598174ff71c8c598
Nishioeda <129241536+Nishioeda@users.noreply.github.com>
2023-06-22 12:46:15 +0900

    kadai10

---
+++ src/main/java/jp/te4a/spring/boot/myapp10/BookBean.java
@@ -0,0 +1,35 @@
+package jp.te4a.spring.boot.myapp10;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "books")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookBean {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "writter")
+    private String writter;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "price")
+    private Integer price;
+}
