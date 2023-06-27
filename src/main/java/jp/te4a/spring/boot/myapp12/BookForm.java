7b43df967af19f41546ea3d69b33a2daedf22c75
Nishioeda <129241536+Nishioeda@users.noreply.github.com>
2023-06-27 13:12:59 +0900

    kadai12

--- src/main/java/jp/te4a/spring/boot/myapp12/BookForm.java
+++ src/main/java/jp/te4a/spring/boot/myapp12/BookForm.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp11;
+package jp.te4a.spring.boot.myapp12;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -16,6 +16,7 @@ public class BookForm {
     private String title;
  
     @Size(min=3, max=20)
+    @Writter(ok = "山本たろう")
     private String writter;
     private String publisher;
 
